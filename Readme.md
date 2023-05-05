9ffbbb314069c91309d191c9b20abed6153ed410
Oscar Sancheze <oscarricardosan@gmail.com>
2023-05-05 18:53:36 -0500

    Codigo enversi[on beta terminado

--- Readme.md
+++ Readme.md
@@ -5,6 +5,16 @@ para el uso del broker de mensajería Kafka.
 
 En este proyecto probamos la capacidad para consumir mensajes de un broker kafka.
 
+# Logs con Grafana Loki
+
+En este caso usaremos el paquete de rust log4rs para guardar el log en un archvo y a promtail para enviar estos archivosa Grafana Loki.
+
+Para log4rs se configura desde el archivo main dentro del método setup_log.
+
+Para promtail lo debemos configurar como un nuevo servicio y el archivo de configuración esta en .devops/docker/develop/promtail_config.yml.
+La carpeta en la que esta el log de la aplicación se monta como volumen en promtail para que los archivos se comuniquen de forma correcta.
+
+
 # Docker
 
 Para levantar la aplicación en entorno de desarrollo ejecutar:
@@ -18,6 +28,8 @@ docker exec -it kafka-rust-consumer bash
 
 #Ejecutar app
 docker exec -it kafka-rust-consumer cargo run
+docker exec -it kafka-rust-consumer cargo run --release
+
 ```
 
 # Compilar
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 use kafka::error::Error as KafkaError;
-use std::str;
+use std::{fs, str};
+
+use log::{debug, error, info, trace, warn, LevelFilter, SetLoggerError};
+use log4rs::{
+    append::{
+        console::{ConsoleAppender, Target},
+        file::FileAppender,
+    },
+    config::{Appender, Config, Root},
+    encode::pattern::PatternEncoder,
+    filter::threshold::ThresholdFilter,
+};
+
+
+use native_tls::{Certificate, TlsConnector};
+use postgres::{Client, NoTls};
+use postgres_native_tls::MakeTlsConnector;
 
 
 /// This program demonstrates consuming messages through a `Consumer`.
@@ -8,10 +24,11 @@ use std::str;
 /// that messages must be marked and committed as consumed to ensure
 /// only once delivery.
 fn main() {
-    env_logger::init();
 
-    let broker = "haproxy:9095".to_owned();
-    let topic = "topic-test".to_owned();
+    setup_log();
+
+    let broker = "kafka-haproxy:9095".to_owned();
+    let topic = "compras-ejecutadas".to_owned();
     let group = "rust-group".to_owned();
 
     if let Err(e) = consume_messages(group, topic, vec![broker]) {
@@ -20,6 +37,21 @@ fn main() {
 }
 
 fn consume_messages(group: String, topic: String, brokers: Vec<String>) -> Result<(), KafkaError> {
+
+    let cert = fs::read("db-certificate-cockroach.crt").unwrap();//es el mismo ca.crt
+    let cert = Certificate::from_pem(&cert).unwrap();
+
+    let connector = TlsConnector::builder()
+        .add_root_certificate(cert)
+        .build()
+        .unwrap();
+
+    let connector = MakeTlsConnector::new(connector);
+    let mut client= Client::connect(
+        "postgresql://savne:password@cockroach-haproxy:26257/defaultdb", connector
+    ).unwrap();
+
+
     let mut con = Consumer::from_hosts(brokers)
         .with_topic(topic)
         .with_group(group)
@@ -36,16 +68,65 @@ fn consume_messages(group: String, topic: String, brokers: Vec<String>) -> Resul
 
         for ms in mss.iter() {
             for m in ms.messages() {
-                println!(
+
+                let value= format!(
                     "{}:{}@{}: {:?}",
                     ms.topic(),
                     ms.partition(),
                     m.offset,
                     str::from_utf8(m.value)
                 );
+                info!("recibido {}", value);
+
+                let query_result= client.query_one("
+                    INSERT INTO events (type, data)
+                    VALUES ('consumer', $1)
+                    returning id", &[ &value],
+                ).unwrap();
             }
             let _ = con.consume_messageset(ms);
+            con.commit_consumed()?;//Si no se ejecuta esta línea el mensaje no se marca como consumido
         }
-        con.commit_consumed()?;//Si no se ejecuta esta línea el mensaje no se marca como consumido
     }
+}
+
+fn setup_log() {
+
+    let level = log::LevelFilter::Info;
+    let file_path = "./log/consumer.log";
+
+    // Build a stderr logger.
+    let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
+
+    // Logging to log file.
+    let logfile = FileAppender::builder()
+        // Pattern: https://docs.rs/log4rs/*/log4rs/encode/pattern/index.html
+        // Especifica como quiero que se guarde en el archivo
+        .encoder(Box::new(PatternEncoder::new("{d} {l} {f} {m}\n")))
+        .build(file_path)
+        .unwrap();
+
+    // Log Trace level output to file where trace is the default level
+    // and the programmatically specified level to stderr.
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .appender(
+            Appender::builder()
+                .filter(Box::new(ThresholdFilter::new(level)))
+                .build("stderr", Box::new(stderr)),
+        )
+        .build(
+            Root::builder()
+                .appender("logfile")
+                .appender("stderr")
+                .build(LevelFilter::Info),
+        )
+        .unwrap();
+
+    // Use this to change log levels at runtime.
+    // This means you can change the default log level to trace
+    // if you are trying to debug an issue and need more logs on then turn it off
+    // once you are done.
+    let _handle = log4rs::init_config(config);
+
 }
